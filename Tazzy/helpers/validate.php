bd51eebbc2dcd8511a971151c83c593d649eff7b
tawanda <tawanda.nyakudjga@gmail.com>
2016-01-19 14:16:26 +0800

    add new rules

--- Tazzy/helpers/validate.php
+++ Tazzy/helpers/validate.php
@@ -39,6 +39,20 @@
                                     $this->_errors[$item]="{$item} already exist, choose another {$item}.";
                                 }
                                 break;
+                            case 'email':
+                                if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                                    $this->_errors[$item]="not a valid {$item}.";
+                                }
+                                break;
+                            case 'url':
+                                if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                                    $this->_errors[$item]="{$rule_value} not a valid {$item}."; 
+                                }
+                                break;
+                            case 'integer'
+                                if(!is_int($value)){
+                                    $this->_errors[$item]="not a valid {$item}.";    
+                                }
                         }
                     }
                 }
